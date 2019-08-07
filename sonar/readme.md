2bd8639a3633d4f2c9215988d9119fd7d0fe7d8c
yangjiandong <youngjiandong@sina.cn>
2019-08-07 11:02:00 +0800

    sonar

---
+++ sonar/readme.md
@@ -0,0 +1,7 @@
+sonarqube
+---
+
+## docker in centos7
+
+- `mkidr my`, data,logs,conf, extensions
+- `chmod -vR 777 ./my`
--- sonar/run.sh
+++ sonar/run.sh
@@ -1,7 +1,8 @@
 docker run -d --name sonarqube \
     -p 9000:9000 \
-    -v ./my/conf:/opt/sonarqube/conf \
-    -v ./my/data:/opt/sonarqube/data \
-    -v ./my/logs:/opt/sonarqube/logs \
-    -v ./my/extensions:/opt/sonarqube/extensions \
+    --privileged=true \
+    -v $(pwd)/my/conf:/opt/sonarqube/conf \
+    -v $(pwd)/my/data:/opt/sonarqube/data \
+    -v $(pwd)/my/logs:/opt/sonarqube/logs \
+    -v $(pwd)/my/extensions:/opt/sonarqube/extensions \
     sonarqube:lts
