4e59b40794e072489ffd44f3cc67cce617000dbe
yangjiandong <youngjiandong@sina.cn>
2019-10-18 14:15:05 +0800

    pytorch

---
+++ python3base/pytorch/run.sh
@@ -0,0 +1,8 @@
+
+docker run -p 8888:8888 \
+    --name jub -d \
+    --privileged \
+    -v ${PWD}/work:/work \
+    -e PASSWORD=123 \
+    -e LANG=C.UTF-8 -e LC_ALL=C.UTF-8 \
+    one/pytorch sh /work/run_jupyter.sh
