ecf7cfca1a5d5f40311687a94d1b537ea36f089f
yangjiandong <youngjiandong@sina.cn>
2019-11-22 16:55:55 +0800

    x

--- mysql/5.7/run.sh
+++ mysql/5.7/run.sh
@@ -2,13 +2,12 @@
 # max-connections 在 docker-compose 方式下通过 my.conf 配置有效
 docker run --name mysql5.7 \
     -v $(pwd)/mysql5.7:/var/lib/mysql \
-    -v $(pwd)/mysql5.7/run:/var/run/mysqld \
+    -v $(pwd)/run:/var/run/mysqld \
     -v $(pwd)/conf:/etc/mysql \
     -p 3306:3306 \
     -e 'MYSQL_ROOT_PASSWORD=123' \
     -d \
     one/mysql:5.7
-    --max-connections=500
 
 # on centos7
 # docker run --name mysql \
