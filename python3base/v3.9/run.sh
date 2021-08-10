dccba925c5e1da4d8d609a2d78423b170d34ed73
yangjiandong <youngjiandong@sina.cn>
2021-08-10 16:26:30 +0800

    [update] py 3.9 add demo dir

--- python3base/v3.9/run.sh
+++ python3base/v3.9/run.sh
@@ -1,6 +1,6 @@
 
 docker run -p 8888:8888 \
-    -v ${PWD}/work:/work \
+    -v ${PWD}/demo:/work \
     -e PASSWORD=123 \
     -e LANG=C.UTF-8 -e LC_ALL=C.UTF-8 \
     --rm -it one/py:3.9 /bin/bash
